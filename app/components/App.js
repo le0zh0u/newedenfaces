287854b13ff527a589f1f0f895780db2c0e420f2
le0zh0u <492753662@qq.com>
2016-08-24 22:07:57 +0800

    add route in front-end

---
+++ app/components/App.js
@@ -0,0 +1,19 @@
+/**
+ * Created by zhouchunjie on 16/8/24.
+ */
+import React from "react";
+import {RouteHandler} from "react-router";
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+            <RouteHandler />
+            </div>
+    )
+        ;
+    }
+}
+
+export default App;
+
---
+++ app/components/Home.js
@@ -0,0 +1,16 @@
+/**
+ * Created by zhouchunjie on 16/8/24.
+ */
+import React from 'react';
+
+class Home extends React.Component{
+    render(){
+        return (
+            <dev className="'alert alert-info">
+                Hello from Home Component
+            </dev>
+        );
+    }
+}
+
+export default Home;
